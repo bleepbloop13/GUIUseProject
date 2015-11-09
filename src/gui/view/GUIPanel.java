a3aaab3a391ad7a67d84207c4aff0ef05d2dd56c
bleepbloop13 <rbak9218@740s-pl-161.local>
2015-11-09 13:23:50 -0700

    Added helper methods

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,19 +1,58 @@
 package gui.view;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 import gui.controller.GUIController;
 
 /**
  * 
  * @author Rory Baker
- * @version 0.1 Nov 9, 2015 Basic constructor setup.
+ * @version 0.2 Nov 9, 2015 Added setupPanel Helper method.
  */
 public class GUIPanel extends JPanel
 {
 	private GUIController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		firstButton = new JButton("Do not click the button");
+		firstTextField = new JTextField("you can type words here");
+
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	/**
+	 * Loads content into the panel
+	 */
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
+	}
+
+	/**
+	 * Specifies the location of elements on the panel
+	 */
+	private void setupLayout()
+	{
+		
+	}
+
+	/**
+	 * Sets up the listeners for any buttons
+	 */
+	private void setupListeners()
+	{
+
 	}
 }
